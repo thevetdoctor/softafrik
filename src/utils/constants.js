72facdab4d65ea881dcf0bd3ead470e61c9b13be
obafemi oderanti <thevetdoctor@gmail.com>
2025-03-30 14:05:19 +0100

    update mail service url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const mailServiceUrl = process.env.MAIL_SERVICE_URL ?? 'http://178.128.32.101:3100';
+export const mailServiceUrl = process.env.MAIL_SERVICE_URL ?? 'https://mail.softafrik.com';
