6032df3967eaa724d8e61311c5cabf5707376515
obafemi oderanti <thevetdoctor@gmail.com>
2025-04-08 01:23:20 +0100

    update helmet

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 const helmet = require('helmet');
-app.use(helmet());
+// app.use(helmet());
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    connectSrc: ["'self'", "https://mail.softafrik.com"],
+  },
+}));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, '../build')));
 
